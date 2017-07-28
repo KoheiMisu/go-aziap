4b0c4f2370f3297236330ec4797303a00126914b
misu <misu@fullspeed.co.jp>
2017-07-28 22:10:26 +0900

    util

---
+++ util/util_01.go
@@ -0,0 +1,58 @@
+package util
+
+import (
+	"reflect"
+)
+
+func Max(target []int) int {
+	var result int
+
+	for _, val := range target {
+		if result < val {
+			result = val
+		}
+	}
+
+	return result
+}
+
+func Min(target []int) int {
+	var result int
+
+	for _, val := range target {
+		if result > val {
+			result = val
+		}
+	}
+
+	return result
+}
+
+func InArray(target interface{}, arr []interface{}) (exists bool) {
+	exists = false
+
+	switch reflect.TypeOf(arr).Kind() {
+		case reflect.Slice:
+			s := reflect.ValueOf(arr)
+			len := s.Len()
+
+			for i := 0; i < len; i++ {
+				if reflect.DeepEqual(target, s.Index(i).Interface()) == true {
+					exists = true
+					return
+				}
+			}
+	}
+
+	return
+}
+
+func ArrayFill(start, end int, value interface{}) []interface{} {
+	var result []interface{}
+
+	for i := start; i < end; i++ {
+		result = append(result, value)
+	}
+
+	return result
+}
